c0dba23c1d8fa656d6a608498955d2a1abf888ea
KalaivananKannan <kkalaivanan2@gmail.com>
2026-08-18 13:00:18 +0530

    Made changes to regexppwd to run the output

--- regexppwd.py
+++ regexppwd.py
@@ -1,27 +1,24 @@
 import re
-
-def register():
-    username=input("Enter the username to register: ")
-    password=input("Enter the password to register: ")
-    print("Password Rules: Must be 8 to 15 characters include 1number, 1lowercase, 1uppercase, 1special character.")
-
-    if (8<=len(password)<=15 and
-        len(re.findall("[a-z]", password)) > 0 and
-        len(re.findall("[A-Z]", password)) > 0 and
-        len(re.findall("[0-9]", password)) > 0 and
-        len(re.findall("[^a-zA-Z0-9]", password)) > 0):
-
-        print("Password registered")
-    else:
-        print("Invalid password.")
-
+    
 while True:
     choice=int(input("1. Register || 2. Login || 3. Exit"))
     if choice==3:
         print("Thank you")
         break
     elif choice==1:
-        register()
+        username=input("Enter the username to register: ")
+        password=input("Enter the password to register: ")
+        
+        if (8<=len(password)<=15 and
+            len(re.findall("[a-z]", password)) > 0 and
+            len(re.findall("[A-Z]", password)) > 0 and
+            len(re.findall("[0-9]", password)) > 0 and
+            len(re.findall("[^a-zA-Z0-9]", password)) > 0):
+
+            print("Password registered")
+        else:
+            print("Invalid password. Must be 8 to 15 characters include 1number, 1lowercase, 1uppercase, 1special character.")
+
     elif choice==2:
         us=input("Enter the username to login: ")
         pa=input("Enter the password to login: ")
@@ -30,5 +27,3 @@ while True:
             print("Login success")
         else:
             print("Login failure")
-
-
